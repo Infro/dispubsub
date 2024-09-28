8333e23d392bd9ab10415d29d39d8311e9367fa4
John B Heilman IV <1735575+Infro@users.noreply.github.com>
2024-09-28 15:40:32 -0700

    Fixing Things

--- main.js
+++ main.js
@@ -2,29 +2,31 @@ var orderDetails = localStorage.getItem("orderDetails") || '[]';
 orderDetails = JSON.parse(orderDetails);
 orderDetails.push(Object.fromEntries(new URLSearchParams(location.search)));
 
-const orderDetailsTable = document.getElementById("orderDetails");
-orderDetails.forEach(order => {
-    const row = document.createElement("tr");
-    row.className = "clickToOrder";
-    row.innerHTML = `
-        <td>${order.orderExecutionType}</td>
-        <td>${order.orderPurchaseType}</td>
-        <td>${order.symbol}</td>
-        <td>${order.strikePrice}</td>
-        <td>${order.price}</td>
-        <td>${order.quantity}</td>
-        <td>${order.quantity * order.price * order.leverage}</td>
-        <td>${order.expiration.substring(0,10)}</td>
-    `;
-    row.data = order;
-    orderDetailsTable.appendChild(row);
-});
-
 //jQuery onReady
 $(function() {
+    const orderDetailsTable2 = document.getElementById("orderDetails");
+    orderDetails.forEach(order => {
+        const row = document.createElement("tr");
+        row.className = "clickToOrder";
+        row.innerHTML = `
+            <td>${order.orderExecutionType}</td>
+            <td>${order.orderPurchaseType}</td>
+            <td>${order.symbol}</td>
+            <td>${order.strikePrice}</td>
+            <td>${order.price}</td>
+            <td>${order.quantity}</td>
+            <td>${order.quantity * order.price * order.leverage}</td>
+            <td>${(order.expiration ? order.expiration.substring(0,10) : "")}</td>
+        `;
+        row.data = order;
+        orderDetailsTable2.appendChild(row);
+    });
+
     $('#orderDetails tr.clickToOrder').click(function (el) {
         const order = this.data.fullOrder;
         // copy order to clipboard
+        // Url decode the order
+        order = decodeURIComponent(order);
         navigator.clipboard.writeText(order);
         if (localStorage.getItem("infrotrade") === "true") {
             window.location.href = "infrotrade://order/" + order;
